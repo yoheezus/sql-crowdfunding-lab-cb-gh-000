9b6d76feed52b6ca240e0ede062e45316f3465a2
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-25 18:12:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT users.name, SUM(pledges.amount) AS all_pledges FROM users INNER JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY all_pledges ASC"
-endpledges.amount
+end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, pledges.amount FROM projects INNER JOIN pledges ON projects.id = pledges.project_id;"
